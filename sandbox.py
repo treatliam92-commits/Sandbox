9b6373495badb8b316508356c8a6fe26d36c5983
Darkining__ <treatliam92@gmail.com>
2026-01-06 14:12:01 -0500

    Added more variables
    Added more variables with user input

--- sandbox.py
+++ sandbox.py
@@ -7,3 +7,9 @@ year = 2026
 name = "Liam Treat"
 age = 17
 print ("My name is ",name,"it is the year of",year,"and i am",age,"years old.")
+print("")
+
+Year = input("what year is it: ")
+Name = input("what is your name: ")
+age = input("How old are you: ")
+print("\nWelcome",name,"it is the year",year + ". You are currently",age," years old")
